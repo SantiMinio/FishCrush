db59c25a5c271c50556001617996c5a744143159
SantiMinio <santiago.mino@davinci.edu.ar>
2021-01-26 05:56:37 -0300

    Más modelos de movimiento de pececitos

---
+++ Assets/Scripts/Peces/CircleMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMove : MovementModule
+{
+    [SerializeField] float spacingBetweenWaypoints = 2;
+    [SerializeField] float radious = 3;
+    [SerializeField] float timeToNextWaypoint = 1;
+    [SerializeField] LayerMask mask = 1 << 0;
+
+    float timer;
+    [SerializeField] int index;
+    bool inWaypont = false;
+
+    public override void Move()
+    {
+        if (inWaypont)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeToNextWaypoint)
+            {
+                timer = 0;
+                inWaypont = false;
+            }
+        }
+        else
+        {
+            transform.forward = Vector3.Lerp(transform.forward, (myWaypoints[index] - transform.position).normalized, Time.deltaTime * rotationSpeed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, myWaypoints[index]) < 0.2f)
+            {
+                inWaypont = true;
+                index += 1;
+
+                if (index >= myWaypoints.Length) index = 1;
+            }
+        }
+    }
+
+    protected override void SetWaypointsAbstract()
+    {
+        RaycastHit hit;
+
+        myWaypoints = new Vector3[8];
+        Vector3 diagonalOne = transform.right + transform.forward;
+        Vector3 diagonalTwo = transform.right - transform.forward;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, radious, mask)) myWaypoints[0] = hit.point + transform.forward * -1;
+        else myWaypoints[0] = transform.position + transform.forward * radious;
+
+        if (Physics.Raycast(transform.position, -transform.forward, out hit, radious, mask)) myWaypoints[4] = hit.point + transform.forward * 1;
+        else myWaypoints[4] = transform.position - transform.forward * radious;
+
+        if (Physics.Raycast(transform.position, transform.right, out hit, radious, mask)) myWaypoints[2] = hit.point + transform.right * -1;
+        else myWaypoints[2] = transform.position + transform.right * radious;
+
+        if (Physics.Raycast(transform.position, -transform.right, out hit, radious, mask)) myWaypoints[6] = hit.point + transform.right * 1;
+        else myWaypoints[6] = transform.position - transform.right * radious;
+
+        if (Physics.Raycast(transform.position, diagonalOne, out hit, spacingBetweenWaypoints, mask)) myWaypoints[1] = hit.point + diagonalOne * -1;
+        else myWaypoints[1] = transform.position + diagonalOne * spacingBetweenWaypoints;
+
+        if (Physics.Raycast(transform.position, -diagonalOne, out hit, spacingBetweenWaypoints, mask)) myWaypoints[5] = hit.point + diagonalOne * 1;
+        else myWaypoints[5] = transform.position - diagonalOne * spacingBetweenWaypoints;
+
+        if (Physics.Raycast(transform.position, diagonalTwo, out hit, spacingBetweenWaypoints, mask)) myWaypoints[3] = hit.point + diagonalTwo * -1;
+        else myWaypoints[3] = transform.position + diagonalTwo * spacingBetweenWaypoints;
+
+        if (Physics.Raycast(transform.position, -diagonalTwo, out hit, spacingBetweenWaypoints, mask)) myWaypoints[7] = hit.point + diagonalTwo * 1;
+        else myWaypoints[7] = transform.position - diagonalTwo * spacingBetweenWaypoints;
+
+        index = 0;
+        inWaypont = false;
+        timer = 0;
+    }
+}
---
+++ Assets/Scripts/Peces/RandomMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MovementModule
+{
+    [SerializeField] float radiousToSearchWaypoints = 4;
+    [SerializeField] int waypointsAmmount = 4;
+    [SerializeField] float timeToNextWaypoint = 1;
+    [SerializeField] LayerMask mask = 1 << 0;
+
+    float timer;
+    [SerializeField] int index;
+    bool inWaypont = false;
+
+    Vector3 currentWaypoint;
+
+    public override void Move()
+    {
+        if (inWaypont)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeToNextWaypoint)
+            {
+                timer = 0;
+                inWaypont = false;
+            }
+        }
+        else
+        {
+            transform.forward = Vector3.Lerp(transform.forward, (currentWaypoint - transform.position).normalized, Time.deltaTime * rotationSpeed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+            Debug.Log(currentWaypoint);
+
+            if (Vector3.Distance(transform.position, myWaypoints[index]) < 0.5f)
+            {
+                int indexRandom = Random.Range(0, myWaypoints.Length);
+                Vector3 temp = myWaypoints[indexRandom];
+                myWaypoints[indexRandom] = currentWaypoint;
+                currentWaypoint = temp;
+                inWaypont = true;
+            }
+        }
+    }
+
+    protected override void SetWaypointsAbstract()
+    {
+        myWaypoints = new Vector3[waypointsAmmount - 1];
+
+        float zMin = transform.position.z - radiousToSearchWaypoints;
+        float zMax = transform.position.z + radiousToSearchWaypoints;
+        float xMin = transform.position.x - radiousToSearchWaypoints;
+        float xMax = transform.position.x + radiousToSearchWaypoints;
+
+        for (int i = 0; i < waypointsAmmount; i++)
+        {
+            if (i >= myWaypoints.Length) { currentWaypoint = new Vector3(Random.Range(xMin, xMax), transform.position.y, Random.Range(zMin, zMax)); break; }
+            myWaypoints[i] = new Vector3(Random.Range(xMin, xMax), transform.position.y, Random.Range(zMin, zMax));
+        }
+
+        index = 0;
+        inWaypont = false;
+        timer = 0;
+    }
+}

9de9781fef4147e52499b4f5ecd0c2980f8aad3c
SantiMinio <santiago.mino@davinci.edu.ar>
2021-02-09 21:50:22 -0300

    El cebo ya no se va a la goma pero sigue mal

--- Assets/Scripts/Bait.cs
+++ Assets/Scripts/Bait.cs
@@ -8,71 +8,75 @@ public class Bait : MonoBehaviour, IUpdate
     bool sale;
     bool returning;
     Vector3 myInitialPos;
-    Vector3 truePos;
+    Vector3 targetPos;
+    Vector3 trueInitialPos;
     Transform myParent;
-    Rigidbody rb;
 
     Action ReachTargetPos;
     Action ReachInitialPos;
 
-    [SerializeField] float potency = 5;
+    [SerializeField] float minSpeed = 0.5f;
+    [SerializeField] float maxSpeed = 1;
+    [SerializeField] float acceleration = 2;
+    float currentSpeed = 0;
+    float currentLerping = 0;
 
     private void Awake()
     {
         myParent = transform.parent;
-        rb = GetComponent<Rigidbody>();
-        truePos = transform.localPosition;
     }
 
     public void OnUpdate()
     {
         if (returning)
         {
+            currentLerping += currentSpeed * Time.deltaTime;
+            if (currentSpeed < maxSpeed) currentSpeed = Mathf.Clamp(currentSpeed + Time.deltaTime * acceleration, minSpeed, maxSpeed);
+            transform.position = Vector3.Lerp(targetPos, myInitialPos, currentLerping);
 
-            if (Vector3.Distance(myInitialPos, transform.position) <= 0.2f)
+            if (currentLerping >= 1)
             {
-                rb.velocity = Vector3.zero;
-                rb.constraints = RigidbodyConstraints.FreezeAll;
                 ReachInitialPos?.Invoke();
                 transform.SetParent(myParent);
-                transform.localPosition = truePos;
                 returning = false;
+                GameManager.instance.updateManager.DesuscribeToUpdate(this);
+            }
+        }
+        else if (sale)
+        {
+            currentLerping += currentSpeed * Time.deltaTime;
+            if (currentSpeed < maxSpeed) currentSpeed = Mathf.Clamp(currentSpeed + Time.deltaTime * acceleration,minSpeed,maxSpeed);
+            transform.position = Vector3.Lerp(myInitialPos, targetPos, currentLerping);
+
+            if (currentLerping >= 1)
+            {
+                sale = false;
+                ReachTargetPos?.Invoke();
                 GameManager.instance.updateManager.DesuscribeToUpdate(this);
             }
         }
     }
 
-    public void GoToPos(Vector3 targetPos, Action callback)
+    public void GoToPos(Vector3 _targetPos, Action callback)
     {
+        trueInitialPos = transform.localPosition;
         myInitialPos = transform.position;
+        targetPos = _targetPos;
         transform.parent = null;
-        rb.constraints = RigidbodyConstraints.None;
-        rb.constraints = RigidbodyConstraints.FreezeRotation;
         sale = true;
         ReachTargetPos = callback;
-        Vector3 dir = (targetPos - transform.position).normalized;
-        rb.AddForce(dir * potency, ForceMode.Impulse);
+        currentSpeed = minSpeed;
+        currentLerping = 0;
+        GameManager.instance.updateManager.SuscribeToUpdate(this);
     }
 
     public void ReturnBait(Action callback)
     {
         returning = true;
         ReachInitialPos = callback;
-        Vector3 dir = (myInitialPos- transform.position).normalized;
-        rb.AddForce(dir * potency, ForceMode.Impulse);
+        currentSpeed = minSpeed;
+        currentLerping = 0;
         GameManager.instance.updateManager.SuscribeToUpdate(this);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (!sale) return;
-
-        if(collision.gameObject.layer == 10)
-        {
-            sale = false;
-            rb.velocity = Vector3.zero;
-            ReachTargetPos?.Invoke();
-        }
     }
 
 }
